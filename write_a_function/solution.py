7a925e1b576f2ab3d3e0c31f45650a18950b8940
Neil Arora <gamerlighten@gmail.com>
2026-05-24 20:03:35 -0500

    enhanced leap year code 5/24/26

--- write_a_function/solution.py
+++ write_a_function/solution.py
@@ -1,17 +1,29 @@
 def is_leap(year):
-    leap = False
     
-    if year % 4 == 0:
-        if year % 100 == 0:
-            if year % 400 == 0:
-                leap = True
-            else:
-                pass
-        else:
-            leap = True
-    else:
-        pass
+    if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
+        return True
+    return False
     
-    return leap
-
 year = int(input())
+print(is_leap(year))
+
+
+# def is_leap(year):
+
+#     leap = False
+    
+#     if year % 4 == 0:
+#         if year % 100 == 0:
+#             if year % 400 == 0:
+#                 leap = True
+#             else:
+#                 pass
+#         else:
+#             leap = True
+#     else:
+#         pass
+    
+#     return leap
+
+# year = int(input())
+# print(is_leap(year))
